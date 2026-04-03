f9268dd01c37c5c3485a76d38150b0b39dc48301
therealozp <anhkhang.le0910@gmail.com>
2026-04-03 16:58:24 -0400

    visualizations

--- utils/density_map.py
+++ utils/density_map.py
@@ -3,7 +3,8 @@ import numpy as np
 import os
 
 # Example variables (set your own paths and counts)
-path_density_maps = "/home/khangphuanhle/fingerprint-generator/density_maps"
+# path_density_maps = "/home/khangphuanhle/fingerprint-generator/density_maps"
+path_density_maps = r"D:\code\fingerprint-generator\density_maps"
 density_map_count = 2000
 
 
